86305f39bbbca2a55d47ddde7525c37805b83af1
jorgepaterm <bestmove2022@gmail.com>
2022-06-07 07:43:56 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import imageMenu from './image/menu.jpeg';
+
+function App() {
+  return (
+    <div className="div-menu">
+        <img className='imagen' src={imageMenu} alt='imagen del menu' />
+    </div>
+  );
+}
+
+export default App;
