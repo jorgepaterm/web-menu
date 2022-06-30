449160fdcffd32ad4de1073ed5375958053b0f91
jorgepaterm <bestmove2022@gmail.com>
2022-06-30 13:47:13 +0800

    agregando mas imagenes al menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import imageMenu from './image/menu.png';
+import imageMenu1 from './image/Diapositiva1.PNG';
+import imageMenu2 from './image/Diapositiva2.PNG';
+import imageMenu3 from './image/Diapositiva3.PNG';
 
 function App() {
   return (
     <div className="div-menu">
-        <img className='imagen' src={imageMenu} alt='imagen del menu' />
+        <img className='imagen' src={imageMenu1} alt='imagen del menu 1' />
+        <img className='imagen' src={imageMenu2} alt='imagen del menu 2' />
+        <img className='imagen' src={imageMenu3} alt='imagen del menu 3' />
     </div>
   );
 }
